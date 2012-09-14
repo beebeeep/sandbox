6c8a81a5f847fcd6b1df7a880d529e6a6a17df17
Migalin Danila <migalin@mcn.ru>
2012-09-14 11:44:33 +0400

    changed

--- test.c
+++ test.c
@@ -7,7 +7,6 @@ int main(int argc, char *argv[])
 	unsigned int i;
 	printf("Hello world!\n\n\nargc = %i\n", argc);
 	for(i = 1; i < argc; i++) {
-	//lalala
 		printf("argv[%i] = %s\n", i, argv[i]);
 	}
 	return 0;
